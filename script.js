8c3adc9e5176af650fd0c6fdccd19c7756879b89
Dev-YoungPandas <dev@youngpandas.com>
2025-03-12 09:14:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -146,8 +146,8 @@ const personImages = {
     const nextBg = backgrounds[currentBgIndex === 0 ? 1 : 0];
     nextBg.style.backgroundImage = `url(${imageUrl})`;
     const transitionSpeed = getDeviceType() === 'mobile' ? 0.25 : 0.3;
-    gsap.to(currentBg, { opacity: 0, duration: transitionSpeed,  });
-    gsap.to(nextBg, { opacity: 1, duration: transitionSpeed,  });
+    gsap.to(currentBg, { opacity: 0   });
+    gsap.to(nextBg, { opacity: 1  });
     currentBgIndex = currentBgIndex === 0 ? 1 : 0;
   }
   
